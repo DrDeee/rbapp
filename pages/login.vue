d032d4d8a6d37589b85145e366195ae1630e44a4
Vincent Woltmann <vincent.woltmann@hu-berlin.de>
2021-01-12 16:46:28 +0100

    :bug: Made greeting gender-neutral

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="text-xl flex flex-col justify-center h-screen items-center">
     <h1 class="text-center m-5">
-      Hi Aktivist, herzlich willkommen zur Regio App
+      Hi Aktivisti, herzlich willkommen zur Regio App
     </h1>
     <button class="p-1 px-2 max-w-sm m-5" @click="$auth.loginWith('cloud')">
       Mit Cloud-Account einloggen
