94be5e5f6d48615eb291448d1df5f5f97d6fcb5e
Vincent Woltmann <vincent.woltmann@hu-berlin.de>
2021-02-06 15:43:05 +0100

    :bug: Fixed greata image on tablets

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,14 @@
 <template>
-  <main class="text-xl flex flex-col justify-center h-screen items-center">
-    <h1 class="text-center m-5">
-      Hi Aktivisti, herzlich willkommen zur Regio App
-    </h1>
-    <button class="p-1 px-2 max-w-sm m-5" @click="$auth.loginWith('cloud')">
-      Mit Cloud-Account einloggen
-    </button>
-    <img src="~/assets/greata.jpeg" />
+  <main>
+    <div class="text-xl flex flex-col h-screen items-center">
+      <h1 class="text-center mt-10 m-5">
+        Hi Aktivisti, herzlich willkommen zur Regio App
+      </h1>
+      <button class="p-1 px-2 max-w-sm m-5" @click="$auth.loginWith('cloud')">
+        Mit Cloud-Account einloggen
+      </button>
+      <img src="~/assets/greata.jpeg" class="max-h-full flex-shrink" />
+    </div>
   </main>
 </template>
 
