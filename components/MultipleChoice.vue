1f5456d5193056ee570bf82b807d9005250b942e
Vincent Woltmann <vincent.woltmann@protonmail.com>
2021-07-23 01:27:47 +0200

    Added 24.09 Poll

---
+++ components/MultipleChoice.vue
@@ -0,0 +1,43 @@
+<template>
+  <fieldset>
+    <legend class="mb-3 text-lg">{{ question }}</legend>
+    <div class="flex justify-around">
+      <label v-for="[answer, answerValue] of choices" :key="answer">
+        {{ answer }}
+        <input
+          type="radio"
+          :value="answerValue"
+          :name="question"
+          :model="value"
+          @change="$emit('input', $event.target.value)"
+        />
+      </label>
+    </div>
+    <slot />
+  </fieldset>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class MultipleChoice extends Vue {
+  @Prop({
+    required: true,
+  })
+  question!: String
+
+  @Prop({
+    default: () => [
+      ['Ja', true],
+      ['Nein', false],
+    ],
+  })
+  choices!: Array<[String, any]>
+
+  @Prop({
+    required: true,
+  })
+  value!: any
+}
+</script>
--- pages/_localGroup/poll.vue
+++ pages/_localGroup/poll.vue
@@ -9,200 +9,145 @@
       <form
         v-if="localGroup && poll"
         class="flex flex-col p-1 sm:p-5 max-w-xl mx-auto"
-        @submit.prevent=""
+        @submit.prevent
       >
-        <h4 class="mb-3 text-lg">
-          Welche Inhalte des OG Starterpacks habt ihr und in welchem Bereich
-          wünscht ihr euch mehr Unterstützung?
+        <MultipleChoice
+          v-model="poll.globalStrike"
+          question="Habt ihr vor eine Aktion am globalen Klimastreik (24.9) zu machen? (Details egal)"
+          :choices="[
+            ['Ja', true],
+            ['Nein', false],
+          ]"
+        >
+          <p v-if="poll.globalStrike === 'true'">
+            Dann schnell damit in die
+            <a href="https://fffutu.re/2409-schnell"
+              >Schnellregistrierung https://fffutu.re/2409-schnell</a
+            >
+          </p>
+        </MultipleChoice>
+        <MultipleChoice
+          v-model="poll.orderedMobi"
+          question="Habt ihr schon Mobimaterial für den 24.09 bestellt?"
+          :choices="[
+            ['Ja', true],
+            ['Nein', false],
+          ]"
+        >
+          <p v-if="poll.orderedMobi === 'false'">
+            Dann mal ab dafür:
+            <a href="https://fffutu.re/mobimaterial"
+              >https://fffutu.re/mobimaterial</a
+            >
+          </p>
+        </MultipleChoice>
+        <MultipleChoice
+          v-model="poll.orderedEquipment"
+          question="Habt ihr schon Technik bestellt?"
+          :choices="[
+            ['Ja', 'alreadyOrdered'],
+            ['Nein und brauchen wir nicht', 'notNeeded'],
+            ['Nein und brauchen wir', 'needed'],
+          ]"
+        >
+          <p v-if="poll.orderedEquipment === 'needed'">
+            Dann bestellt hier:
+            <a href="https://fffutu.re/technikbedarf"
+              >fffutu.re/technikbedarf</a
+            >
+          </p>
+        </MultipleChoice>
+        <MultipleChoice
+          v-model="poll.profileGenerator"
+          question="Habt ihr schon unseren Profilbildgenerator geteilt?"
+          :choices="[
+            ['Ja', true],
+            ['Nein', false],
+          ]"
+        >
+          <p v-if="poll.profileGenerator === 'false'">
+            Dann teilen, teilen, teilen:
+            <a href="https://fffutu.re/profilbildfuersklima"
+              >fffutu.re/profilbildfuersklima</a
+            >
+          </p>
+        </MultipleChoice>
+        <MultipleChoice
+          v-model="poll.folder"
+          question="Kennt ihr schon unseren Linktree zum globalen Klimastreik?"
+          :choices="[
+            ['Ja', true],
+            ['Nein', false],
+          ]"
+        >
+          <p v-if="poll.folder === 'false'">
+            Dann tobt euch aus:
+            <a href="https://ffftre.es/global_strike_24_09"
+              >ffftre.es/global_strike_24_09</a
+            >
+          </p>
+        </MultipleChoice>
+        <h4>
+          Bei welchem Workshop vor der Bundestagswahl hättet ihr Interesse
+          teilzunehmen?
         </h4>
-        <table class="w-full flex flex-col">
-          <thead class="flex justify-end sm:justify-start w-full">
-            <th class="mr-2 flex-grow sm:flex-none sm:w-40 sm:mr-0"></th>
-            <th class="block w-12 mr-2 text-center">Haben Wir</th>
-            <th class="block w-20 text-center">Brauchen Wir</th>
-          </thead>
-          <tbody>
-            <tr
-              class="flex flex-wrap justify-end sm:justify-start items-center sm:h-8"
+        <MultipleChoice
+          v-model="poll.strategy"
+          question="Politische Strategie für Aktivistis"
+        />
+        <MultipleChoice
+          v-model="poll.strategy"
+          question="Teamorga für Aktivistis"
+        />
+        <MultipleChoice
+          v-model="poll.pressBeginner"
+          question="Presse für Anfänger:innen"
+        />
+        <MultipleChoice
+          v-model="poll.pressAdvanced"
+          question="Presse für Fortgeschrittene"
+        />
+        <MultipleChoice
+          v-model="poll.howToFFF"
+          question="How to FFF - Grundlagen für neue und junge Ortsgruppen"
+        />
+        <MultipleChoice
+          v-model="poll.skillShare"
+          question="Skillsharing Ausbildung"
+        />
+        <MultipleChoice
+          v-model="poll.socialMediaWS"
+          question="Social-Media Workshops"
+        />
+        <MultipleChoice
+          v-model="poll.politicalTalks"
+          question="Politiker:innen Gespräche"
+        />
+        <label>
+          An welchen Tagen und zu welchen Uhrzeiten habt ihr tendenziell am
+          ehesten Zeit und Lust auf einen Workshop?
+          <textarea class="w-full"></textarea>
+        </label>
+        <MultipleChoice
+          v-model="poll.stepForStep"
+          question="Kennt ihr unseren Schritt-für-Schritt Aktionsplanungsleitfaden?"
+        >
+          <p v-if="poll.stepForStep === 'false'">
+            Dann los:
+            <a href="https://fffutu.re/schritt-fuer-schritt"
+              >fffutu.re/schritt-fuer-schritt</a
             >
-              <td class="flex-grow sm:flex-none sm:w-40">
-                Sticker und Plakate:
-              </td>
-              <td class="flex items-center justify-center w-12 mr-2">
-                <input
-                  v-model="poll.hasMobiStuff"
-                  type="radio"
-                  name="hasMobiStuff"
-                  :value="true"
-                />
-              </td>
-              <td class="flex items-center justify-center w-20">
-                <input
-                  v-model="poll.hasMobiStuff"
-                  type="radio"
-                  name="hasMobiStuff"
-                  :value="false"
-                />
-              </td>
-              <td
-                v-if="poll.hasMobiStuff === false"
-                class="w-full sm:w-auto text-center flex justify-end"
-              >
-                <a
-                  href="https://forms.gle/peK8Mf9EJWx21Yax6"
-                  class="text-blue-500 mr-2"
-                  target="_blank"
-                >
-                  Bestell-link
-                </a>
-                <div class="flex">
-                  <button
-                    v-clipboard="'https://forms.gle/peK8Mf9EJWx21Yax6'"
-                    class="mr-5"
-                  >
-                    <font-awesome-icon icon="copy" />
-                  </button>
-                  <button
-                    v-if="$util.hasShare()"
-                    @click.prevent="
-                      $util.share({
-                        url: 'https://forms.gle/peK8Mf9EJWx21Yax6',
-                        title: 'Plakate & Sticker bestellen',
-                      })
-                    "
-                  >
-                    <font-awesome-icon icon="share" />
-                  </button>
-                </div>
-              </td>
-            </tr>
-            <tr
-              class="flex flex-wrap justify-end items-center sm:justify-start sm:h-8"
-            >
-              <td class="flex-grow sm:flex-none sm:w-40">
-                Ordner:innenbinden:
-              </td>
-              <td class="flex items-center justify-center w-12 mr-2">
-                <input
-                  v-model="poll.hasArmbands"
-                  type="radio"
-                  name="hasArmbands"
-                  :value="true"
-                />
-              </td>
-              <td class="flex items-center justify-center w-20">
-                <input
-                  v-model="poll.hasArmbands"
-                  type="radio"
-                  name="hasArmbands"
-                  :value="false"
-                />
-              </td>
-              <td
-                v-if="poll.hasArmbands === false"
-                class="w-full flex justify-end sm:w-auto"
-              >
-                <a
-                  href="https://fffutu.re/"
-                  class="text-blue-500 mr-2"
-                  target="_blank"
-                >
-                  Starterpack bestellen
-                </a>
-                <div class="flex">
-                  <button v-clipboard="'https://fffutu.re/'" class="mr-5">
-                    <font-awesome-icon icon="copy" />
-                  </button>
-                  <button
-                    v-if="$util.hasShare()"
-                    @click.prevent="
-                      $util.share({
-                        url: 'https://fffutu.re/',
-                        title: 'Starterpack für deine OG bestellen',
-                      })
-                    "
-                  >
-                    <font-awesome-icon icon="share" />
-                  </button>
-                </div>
-              </td>
-            </tr>
-            <tr
-              class="flex flex-wrap justify-end items-center sm:justify-start sm:h-8"
-            >
-              <td class="flex-grow sm:flex-none sm:w-40">Technik:</td>
-              <td class="flex items-center justify-center w-12 mr-2">
-                <input
-                  v-model="poll.hasEquipment"
-                  type="radio"
-                  name="hasEquipment"
-                  :value="true"
-                />
-              </td>
-              <td class="flex items-center justify-center w-20">
-                <input
-                  v-model="poll.hasEquipment"
-                  type="radio"
-                  name="hasEquipment"
-                  :value="false"
-                />
-              </td>
-              <!--
-              <td
-                v-if="poll.hasEquipment === false"
-                class="flex w-full justify-end sm:w-auto"
-              >
-                <a
-                  href="https://docs.google.com/forms/d/e/1FAIpQLSfFkBfhHskJhlkfykarrE2MOyv3lbHXNKgCVCaGpFoqZ6Qrrg/viewform"
-                  class="text-blue-500 mr-2"
-                  target="_blank"
-                >
-                  Technikbedarfsumfrage
-                </a>
-                <div class="flex">
-                  <button v-clipboard="'https://fffutu.re/'" class="mr-5">
-                    <font-awesome-icon icon="copy" />
-                  </button>
-                  <button
-                    v-if="$util.hasShare()"
-                    @click.prevent="
-                      $util.share({
-                        url: 'https://fffutu.re/',
-                        title: 'Technik für deine OG bestellen',
-                      })
-                    "
-                  >
-                    <font-awesome-icon icon="share" />
-                  </button>
-                </div>
-              </td>
-              -->
-            </tr>
-          </tbody>
-        </table>
-        <p class="mt-3">
-          Allgemein gibt es noch die Schritt-für-Schritt Anleitung, die das
-          Organisieren einer Demo komplett erklärt:
-          <a
-            href="https://fffutu.re/schritt-fuer-schritt"
-            class="text-blue-500"
-            target="_blank"
-          >
-            https://fffutu.re/schritt-fuer-schritt
-          </a>
-        </p>
-        <div class="flex flex-wrap mt-5 items-center">
-          <h4 class="text-lg self-begin mr-3">
-            Wie groß war eure größte Demo (ca.)?
-          </h4>
-          <div class="flex-grow flex justify-end">
-            <input
-              v-model="poll.biggestStrike"
-              type="number"
-              class="appearance-none w-20"
-            />
-          </div>
-        </div>
+          </p>
+        </MultipleChoice>
+        <MultipleChoice
+          v-model="poll.playbook"
+          question="Kennt ihr unser OG-Playbook mit Tipps und Tricks zur Organisation eurer Ortsgruppe?"
+        >
+          <p v-if="poll.playbook === 'false'">
+            Dann findet ihr das hier:
+            <a href="https://ffftre.es/og-playbook">ffftre.es/og-playbook</a>
+          </p>
+        </MultipleChoice>
         <div
           class="flex flex-col mt-5 mb-3 items-center justify-between flex-wrap"
         >
@@ -260,70 +205,67 @@
             hinzugefügt
           </p>
         </div>
-        <div class="flex flex-wrap pt-5">
-          <h4 class="mb-1 text-lg max-w-sm">
-            Welche SM-Kanäle habt ihr (WA, Insta, ...) und habt ihr sie schon
-            auf die Website eingetragen?
-          </h4>
-          <div class="flex flex-col flex-grow justify-around ml-3">
-            <label class="flex items-center self-end w-32">
-              <input
-                v-model="poll.hasUpToDateSM"
-                type="radio"
-                class="mr-1"
-                name="hasUpToDateSM"
-                :value="true"
-              />
-              Alles aktuell
-            </label>
-            <label class="flex items-center self-end w-32">
-              <input
-                v-model="poll.hasUpToDateSM"
-                type="radio"
-                class="mr-1"
-                name="hasUpToDateSM"
-                :value="false"
-              />
-              Nicht wirklich
-            </label>
-          </div>
-          <p
-            v-if="poll.hasUpToDateSM === false"
-            class="ml-1 mt-1 leading-tight text-center"
-          >
-            Die SM-Kanäle können hier aktualisiert werden:
-            <a
-              href="https://fffutu.re/MessengerEintragen"
-              class="text-blue-600"
-              target="_blank"
+        <h4>Kennt ihr unsere wichtigsten Tools?</h4>
+        <MultipleChoice
+          v-model="poll.demonstrationRegistration"
+          question="Demoanmeldungsgenerator?"
+        >
+          <p v-if="poll.demonstrationRegistration === 'false'">
+            Dann ab dafür:
+            <a href="https://fffutu.re/Demoanmeldungsgenerator"
+              >fffutu.re/Demoanmeldungsgenerator</a
             >
-              https://fffutu.re/MessengerEintragen
-            </a>
           </p>
-        </div>
-        <div class="flex flex-wrap items-center my-5">
-          <h4 class="text-lg mr-3">Werdet ihr beim Großstreik streiken?</h4>
-          <div class="flex-grow flex justify-end">
-            <select v-model="poll.globalStrike" class="max-w-sm self-end">
-              <option :value="null">-</option>
-              <option :value="0">Sicher nicht</option>
-              <option :value="1">Vermutlich nicht</option>
-              <option :value="2">Vermutlich</option>
-              <option :value="3">Auf jeden Fall</option>
-            </select>
-          </div>
-          <p v-if="poll.globalStrike >= 2">
-            Die Aktion sollte schonmal in die Schnellregistrierung eingetragen
-            werden:
-            <a
-              href="https://fffutu.re/1903-schnell"
-              class="text-blue-500"
-              target="_blank"
+        </MultipleChoice>
+        <MultipleChoice v-model="poll.toolpic" question="Toolpic?">
+          <p v-if="poll.toolpic === 'false'">
+            Dann ab dafür:
+            <a href="https://toolpic.de">toolpic.de</a>
+          </p>
+        </MultipleChoice>
+        <MultipleChoice v-model="poll.cloud" question="Cloud?">
+          <p v-if="poll.cloud === 'false'">
+            Dann ab dafür:
+            <a href="https://fffutu.re/cloud_beantragen"
+              >fffutu.re/cloud_beantragen</a
             >
-              https://fffutu.re/1903-schnell
-            </a>
           </p>
-        </div>
+        </MultipleChoice>
+        <MultipleChoice v-model="poll.linktree" question="Linktree?">
+          <p v-if="poll.linktree === 'false'">
+            Dann ab dafür:
+            <a href="https://admin.ffftre.es">admin.ffftr.es</a>
+          </p>
+        </MultipleChoice>
+        <MultipleChoice v-model="poll.linkshortener" question="Linkverkürzer?">
+          <p v-if="poll.linkshortener === 'false'">
+            Dann ab dafür:
+            <a href="https://fffutu.re">fffutu.re</a>
+          </p>
+        </MultipleChoice>
+        <MultipleChoice
+          v-model="poll.playbook"
+          question="Wisst ihr wo ihr Hilfe bekommt wegen Finanzierung eurer Demos?"
+        >
+          <p v-if="poll.playbook === 'false'">
+            Dann findet ihr das hier:
+            <a
+              href="https://ffftre.es/finanzen_finanzierung_von_materialien_und_aktionen"
+              >ffftre.es/finanzen_finanzierung_von_materialien_und_aktionen</a
+            >
+          </p>
+        </MultipleChoice>
+        <MultipleChoice
+          v-model="poll.spenden"
+          question="Sammelt ihr schon Spenden auf euren Demos?"
+        >
+          <p v-if="poll.spenden === 'false'">
+            Dann fangt mal an :) Die Finanz AG gibt dazu bald auch einen
+            Leitfaden raus, an sich aber einfach einmal die Leute anhauen und
+            nach Unterstützung freundlich fragen
+          </p>
+        </MultipleChoice>
+        <p class="text-center">Danke für eure Arbeit &lt;3</p>
       </form>
     </main>
   </div>
@@ -370,3 +312,9 @@ export default class PollView extends Vue {
   }
 }
 </script>
+
+<style lang="postcss" scoped>
+a {
+  @apply text-blue-500;
+}
+</style>
